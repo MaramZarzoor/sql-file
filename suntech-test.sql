87c499660fb9ab10a5ef7727ca75ecfbb75b7a51
Maram-Zarzour <maramzrzoor@gmail.com>
2022-08-19 00:54:04 +0400

    sql-file
    sql file for user table

---
+++ suntech-test.sql
@@ -0,0 +1,10 @@
+CREATE TABLE `database`.`users`(
+`id` INT NOT NULL AUTO_INCREMENT,
+`firstName` VARCHAR(255) NOT NULL,
+`lastName` VARCHAR(255) NOT NULL,
+`email` VARCHAR(255) NOT NULL,
+`address` VARCHAR(255) NOT NULL,
+`contactNumber` VARCHAR(255) NOT NULL,
+`password` VARCHAR(255) NOT NULL,
+PRIMARY KEY(`id`)
+);
